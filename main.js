d364deec29e27e58ea95766e5662c32cd5194a8c
marichemia <91846117+marichemia@users.noreply.github.com>
2022-06-29 17:40:57 +0400

    dsfs
    sdcffsdfdsf

--- main.js
+++ main.js
@@ -11,12 +11,16 @@ menuIcon.addEventListener('click', () => {
 
 const togglebtn = document.getElementById("toggle-button");
 const play_train_txt = document.getElementById("play-train");
+const grid_container = document.querySelector(".grid-container");
 togglebtn.addEventListener('click', function () {
     if (togglebtn.checked == true) {
         console.log('heeeeeeeeey');
         play_train_txt.textContent = "PLAY";
+        grid_container.classList.add("play-mode-color");
+
     } else {
         play_train_txt.textContent = "TRAIN";
+        grid_container.classList.remove("play-mode-color");
         console.log("hiiiiiiiiiii");
     }
 })
