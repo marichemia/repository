e3b413cf0c07eddc447764819904811bee3e925c
marichemia <91846117+marichemia@users.noreply.github.com>
2022-07-11 15:30:23 +0400

    დფგდ
    დფგდგფ

---
+++ animals.js
@@ -0,0 +1,50 @@
+export const animals = [
+    {
+        img: 'dog.png',
+        word: 'dog',
+        translation: 'ძაღლი',
+        audio: 'dogaudio.mp3'
+    },
+    {
+        img: 'giraffe.png',
+        word: 'Giraffe',
+        translation: 'ჟირაფი',
+        audio: 'giraffeaudio.mp3'
+    },
+    {
+        img: 'panda.png',
+        word: 'Panda',
+        translation: 'პანდა',
+        audio: 'pandaaudio.mp3'
+    },
+    {
+        img: 'turtle.png',
+        word: 'Turtle',
+        translation: 'კუ',
+        audio: 'turtleaudio.mp3'
+    },
+    {
+        img: 'seal.png',
+        word: 'Seal',
+        translation: 'სელაპი',
+        audio: 'sealaudio.mp3'
+    },
+    {
+        img: 'elephant.png',
+        word: 'Elephant',
+        translation: 'სპილო',
+        audio: 'elephantaudio.mp3'
+    },
+    {
+        img: 'gorilla.png',
+        word: 'Gorilla',
+        translation: 'გორილა',
+        audio: 'gorillaaudio.mp3'
+    },
+    {
+        img: 'raccoon.png',
+        word: 'Raccoon',
+        translation: 'ენოტი',
+        audio: 'raccoonaudio.mp3'
+    }
+]
